2640fe47888b9690f33e2acc1541573e9aa98971
Erik Olseng <e.olseng@gmail.com>
2019-08-28 16:14:20 +0200

    initial commit

---
+++ src/test/java/no/kristiania/maven/YatzyTest.java
@@ -0,0 +1,14 @@
+package no.kristiania.maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YatzyTest {
+
+    @Test
+    void shouldReturn4(){
+        assertEquals(4, 2+2);
+    }
+
+}
